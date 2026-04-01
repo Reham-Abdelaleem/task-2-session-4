e143a2694ac245e8cb34c13545c6bfff2eb5be28
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-04-01 09:22:59 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import SkillsSection from "@/components/organisms/SkillsSection"
-import NewsletterSection from "@/components/organisms/NewsletterSection"
-import PartnersSection from "@/components/organisms/PartnersSection"
-import PricingSection from "@/components/organisms/PricingSection"
 import Navbar from "@/components/organisms/Navbar"
 import HeroSection from "@/components/organisms/HeroSection"
+import PricingSection from "@/components/organisms/PricingSection"
 export default function page() {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <SkillsSection />
-      <NewsletterSection />
-      <PartnersSection />
       <PricingSection />
     </div>
   );
--- src/components/organisms/PartnersSection.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PartnersSection() {
-   return (
-      <div className='w-full p-6 text-white text-center font-2xl bg-indigo-300'>Partners Section</div>
-   )
-}
