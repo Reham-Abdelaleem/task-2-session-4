776e30e530184e27f49e3387ddc52ba366ee2747
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-04-01 09:55:50 +0200

    style(pricing): center content vertically and horizontally and update section height

--- src/components/organisms/PricingSection.tsx
+++ src/components/organisms/PricingSection.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function PricingSection() {
    return (
-      <div className='w-full p-10 text-white text-center font-2xl bg-indigo-300'>Pricing Section</div>
+      <div className='w-full flex justify-center items-center p-10 text-white text-center font-3xl bg-indigo-300 min-h-[40vh]'>Pricing Section</div>
    )
 }
