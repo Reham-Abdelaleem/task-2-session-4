9f5ac014a0313a215daba6e35e69e2f2e6bedd41
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-04-01 08:21:14 +0200

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import SkillsSection from "@/components/organisms/SkillsSection"
+import NewsletterSection from "@/components/organisms/NewsletterSection"
+import PartnersSection from "@/components/organisms/PartnersSection"
+export default function page() {
+  return (
+    <div>
+      <SkillsSection />
+      <NewsletterSection />
+      <PartnersSection />
+    </div>
+  );
+}
