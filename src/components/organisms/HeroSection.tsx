8fc10c0019aecb898d74b894c9699166e6faaa5c
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-04-01 09:08:39 +0200

    feat(home): add Hero section in home page

---
+++ src/components/organisms/HeroSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HeroSection() {
+   return (
+      <div className='w-full p-8 text-white text-center font-3xl bg-indigo-400'>Hero Section</div>
+   )
+}
