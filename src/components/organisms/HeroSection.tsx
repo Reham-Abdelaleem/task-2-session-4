44d925e143d039121bbe8f5871e76d879b801302
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-04-01 10:26:29 +0200

    style(hero): update hero section bg

--- src/components/organisms/HeroSection.tsx
+++ src/components/organisms/HeroSection.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function HeroSection() {
    return (
-      <div className='w-full p-8 text-white text-center font-3xl bg-indigo-400'>Hero Section</div>
+      <div className='w-full p-8 text-white text-center font-3xl bg-violet-400'>Hero Section</div>
    )
 }
